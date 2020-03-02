001b0cf9b02591c93d7a7cc1e8b6b4a13a1a9077
Rex.Lin <rex.lin@isapsolution.com>
2020-03-02 17:45:00 +0800

    revision

--- MessagePublisher/MessagePublisher.h
+++ MessagePublisher/MessagePublisher.h
@@ -65,12 +65,18 @@ namespace message {
 			auto data_ptr = std::make_shared<std::string>(
 				proto::message_serializer::serialize(message_));
 
-			list_.push(data_ptr);
+			bool need_to_send = false;
+			{
+				boost::unique_lock<boost::shared_mutex> lock(mutex_);
+				need_to_send = list_.empty();
+				list_.push(data_ptr);
+			}
 
-			auto self = shared_from_this();
-			worker_.io_context_.post([this, self]() {
-				do_send();
-			});
+			if (need_to_send) {
+				worker_.io_context_.post([this]() {
+					do_send();
+				});
+			}
 		}
 
 		void set_on_error(OnError on_error) {
@@ -118,19 +124,25 @@ namespace message {
 		}
 
 		void do_send() {
-			if (list_.empty()) return;
-			auto data_ptr = list_.fetch();
+			
+			std::shared_ptr<std::string> data_ptr = nullptr;
+			{
+				boost::unique_lock<boost::shared_mutex> lock(mutex_);
+				if (list_.empty()) return;
+				data_ptr = list_.peek();
+				if (!data_ptr) return;
+			}
 
-			std::vector<boost::asio::const_buffer> buf_vec;
-			uint32_t size = data_ptr->size();
-			buf_vec.emplace_back(boost::asio::buffer(start_code, sizeof(start_code)));
-			buf_vec.emplace_back(boost::asio::buffer(&size, sizeof(uint32_t)));
-			buf_vec.emplace_back(boost::asio::buffer(*data_ptr));
+			auto buf_vec_ptr = std::make_shared<std::vector<boost::asio::const_buffer>>();
+			uint32_t size = data_ptr->size(); 
+			buf_vec_ptr->emplace_back(boost::asio::buffer(start_code, sizeof(start_code)));
+			buf_vec_ptr->emplace_back(boost::asio::buffer(&size, sizeof(uint32_t)));
+			buf_vec_ptr->emplace_back(boost::asio::buffer(*data_ptr));
 			
 			auto self = shared_from_this();
 			boost::asio::async_write(
-				socket_, buf_vec,
-				[data_ptr, self, this](
+				socket_, *buf_vec_ptr,
+				[this, self, buf_vec_ptr](
 					boost::system::error_code ec,
 					std::size_t bytes_transfered) {
 				if (ec) {
@@ -141,10 +153,17 @@ namespace message {
 					return;
 				}
 
-				//logger::debug("Publisher", ++count);
-				/*if (!list_.empty()) {
+				bool need_send_next = false;
+				{
+					boost::unique_lock<boost::shared_mutex> lock(mutex_);
+					//logger::debug("Publisher", ++count);
+					list_.consume_one();
+					need_send_next = !list_.empty();
+				}
+
+				if (need_send_next) {
 					do_send();
-				}*/
+				}
 			});
 		}
 	};
